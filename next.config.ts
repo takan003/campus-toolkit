1ee4e86fe8e8d79670601e7f0f3738314d120cfd
takan003 <takan003@gms.hlgs.hlc.edu.tw>
2026-09-24 07:59:34 +0800

    修正 CSP img-src 缺少 drive.google.com 導致頁尾雲端硬碟圖片破圖；版本更新至 0.100

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const csp = [
   // Next.js 內聯 script + AdSense + Firebase Auth/SDK
   "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://pagead2.googlesyndication.com https://googleads.g.doubleclick.net https://www.googletagmanager.com https://apis.google.com https://www.gstatic.com https://www.google.com",
   "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://www.gstatic.com",
-  "img-src 'self' data: blob: https://pagead2.googlesyndication.com https://googleads.g.doubleclick.net https://www.google.com https://www.gstatic.com https://lh3.googleusercontent.com",
+  "img-src 'self' data: blob: https://pagead2.googlesyndication.com https://googleads.g.doubleclick.net https://www.google.com https://www.gstatic.com https://lh3.googleusercontent.com https://drive.google.com",
   "font-src 'self' data: https://fonts.gstatic.com https://www.gstatic.com",
   "connect-src 'self' https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firestore.googleapis.com https://www.googleapis.com https://pagead2.googlesyndication.com https://firebaseinstallations.googleapis.com https://*.firebaseapp.com https://*.googleapis.com",
   "frame-src 'self' https://accounts.google.com https://googleads.g.doubleclick.net https://pagead2.googlesyndication.com https://*.firebaseapp.com https://*.google.com",
